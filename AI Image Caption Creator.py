1e85407d6485447115152f97d1b07677a3cbe9b4
aarohidhand <150023944+aarohidhand@users.noreply.github.com>
2024-08-22 21:32:59 +0530

    Update AI Image Caption Creator.py

--- AI Image Caption Creator.py
+++ AI Image Caption Creator.py
@@ -4,9 +4,10 @@ from PIL import Image
 processor = BlipProcessor.from_pretrained('Salesforce/blip-image-captioning-base')
 model = BlipForConditionalGeneration.from_pretrained('Salesforce/blip-image-captioning-base')
 
-image = Image.open("path-to-your-image.jpg")
+image = Image.open("egpics/spizzy.jpg")
 
-inputs = processor(image, return_tensors='pt')
+text = 'a photograph of'   ## input text for conditional caption
+inputs = processor(image, text, return_tensors='pt')
 
 outputs = model.generate(**inputs)
 caption = processor.decode(outputs[0],skip_special_tokens=True)
